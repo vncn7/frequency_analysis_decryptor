89de4981102821093f64426aef9298f64fd244f4
vncn7 <vncn7@protonmail.com>
2024-10-03 10:33:12 +0200

    rework

--- frequency_analysis_decryptor.py
+++ frequency_analysis_decryptor.py
@@ -4,6 +4,7 @@ from collections import Counter
 encrypted_text = "LFAJ AJ LFO JADNKO OHECUNLAWH WY Q JADNKO LOML ALJ ZOLLOC LW FQTO KWTOB QHB KWJL LFQH HOTOC LW FQTO KWTOB QL QKK"
 
 # Remove non-alphabetic characters (e.g., spaces) for frequency analysis
+# REWORK THIS
 cleaned_text = ''.join(filter(str.isalpha, encrypted_text))
 
 # Frequency analysis
